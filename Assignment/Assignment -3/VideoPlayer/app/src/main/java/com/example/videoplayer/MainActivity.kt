1dfa447ebbfef10d470708f1dd09c54c5f984750
Aniketh Pratihar <aniketh5660@gmail.com>
2023-07-02 12:32:59 +0530

    Assignments 1,2,3 added

---
+++ Assignment/Assignment -3/VideoPlayer/app/src/main/java/com/example/videoplayer/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.videoplayer
+
+import android.media.browse.MediaBrowser
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.videoplayer.ui.theme.VideoPlayerTheme
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ui.PlayerView
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+           VideoPlayerTheme() {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    VideoPlayer()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun VideoPlayer(){
+    val sampleVideo = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+    val context = LocalContext.current
+    val player = SimpleExoPlayer.Builder(context).build()
+    val playerView = PlayerView(context)
+    val mediaItem = MediaItem.fromUri(sampleVideo)
+    val playWhenReady by rememberSaveable {
+        mutableStateOf(true)
+    }
+    player.setMediaItem(mediaItem)
+    playerView.player = player
+    LaunchedEffect(player) {
+        player.prepare()
+        player.playWhenReady = playWhenReady
+
+    }
+    AndroidView(factory = {
+        playerView
+    })
+}
