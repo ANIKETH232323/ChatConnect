0c6e671fa82f7d5ca4dd2ea53097eba5d0347850
Aniketh Pratihar <aniketh5660@gmail.com>
2023-07-02 21:31:10 +0530

    Updated

--- app/src/main/java/com/example/chatu/welcome/Welcome.kt
+++ app/src/main/java/com/example/chatu/welcome/Welcome.kt
@@ -1,23 +1,16 @@
 package com.example.chatu.welcome
 
-import android.content.Intent
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,12 +22,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.chatu.MainActivity
 import com.example.chatu.R
-import com.example.chatu.nav.Destination.Register
 
 @Composable
 private fun Gradient_Background(
