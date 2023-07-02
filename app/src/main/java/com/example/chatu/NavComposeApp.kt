1d531fc9835303a1017884e7c295a1d6935a59c8
Aniketh Pratihar <aniketh5660@gmail.com>
2023-07-02 21:28:24 +0530

    Updated

--- app/src/main/java/com/example/chatu/NavComposeApp.kt
+++ app/src/main/java/com/example/chatu/NavComposeApp.kt
@@ -1,14 +1,7 @@
 package com.example.chatu
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHost
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
--- app/src/main/java/com/example/chatu/view/chat_menu/HomeViewModel.kt
+++ app/src/main/java/com/example/chatu/view/chat_menu/HomeViewModel.kt
@@ -1,15 +1,9 @@
 package com.example.chatu.view.chat_menu
 
-import android.content.Intent
 import android.util.Log
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.chatu.nav.Action
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
