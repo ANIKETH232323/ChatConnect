33ed069575fa4006ed654c08fccbe7dd7d3d3236
Aniketh Pratihar <aniketh5660@gmail.com>
2023-07-02 21:30:12 +0530

    Updated

--- app/src/main/java/com/example/chatu/view/chat_menu/Special_Function.kt
+++ app/src/main/java/com/example/chatu/view/chat_menu/Special_Function.kt
@@ -2,14 +2,11 @@ package com.example.chatu.view.chat_menu
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -17,44 +14,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.chatu.view.chat_menu.ui.theme.Pink80
 import com.example.chatu.view.chat_menu.ui.theme.Purple40
-
-/**
- * Set of widgets/views which will be used throughout the application.
- * This is used to increase the code usability.
- */
-
-@Composable
-fun Title(title: String) {
-    Text(
-        text = title,
-        fontSize = 30.sp,
-        fontWeight = FontWeight.Bold,
-        modifier = Modifier.fillMaxHeight(0.5f)
-    )
-}
-
-// Different set of buttons in this page
-@Composable
-fun Buttons(title: String, onClick: () -> Unit) {
-    Button(
-        onClick = onClick,
-        colors = ButtonDefaults.buttonColors(
-            contentColor = White
-        ),
-        modifier = Modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(0),
-    ) {
-        Text(
-            text = title
-        )
-    }
-}
 
 //@OptIn(ExperimentalMaterial3Api::class)
 //@Composable
@@ -237,7 +200,4 @@ fun SingleMessage(message: String, isCurrentUser: Boolean) {
         }
 
     }
-
-
-
 }
